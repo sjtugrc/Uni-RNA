dbbacc30f9919a6bcdebdb395ded988c5b1d8d7e
dp_grc <gurc@dp.tech>
2024-03-20 13:08:17 +0800

    delete useless file

--- build_model.py
+++
@@ -1,5 +0,0 @@
-from unirna.config import build_config
-
-path = "/home/wangxi/project/Uni-RNA/unirna_fineturn/model_weight/unirna_L16_E1024_DPRNA500M_STEP400K.pt"
-config = build_config(path)
-config.save_pretrained("config.json")
