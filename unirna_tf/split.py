e5b8df463b27c57097ad9e1ed0aa6e09d88acf33
dp_grc <gurc@dp.tech>
2024-10-29 17:57:46 +0800

    add script for spliting fasta file

---
+++ unirna_tf/split.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+from Bio import SeqIO
+from tqdm import tqdm
+
+
+def split_fasta(input_file, output_dir, num_files):
+    # 计算fasta文件中的序列数目
+    total_records = sum(1 for _ in tqdm(SeqIO.parse(input_file, "fasta")))
+    num_files = int(num_files)
+
+    # 每个文件应该包含的序列数目
+    records_per_file = total_records // num_files
+    if total_records % num_files != 0:
+        records_per_file += 1
+
+    # 创建输出文件夹
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    record_iter = SeqIO.parse(open(input_file), "fasta")
+    for i in tqdm(range(num_files)):
+        filename = os.path.join(output_dir, "split_{}.fasta".format(i))
+        with open(filename, "w") as out_handle:
+            count = 0
+            while count < records_per_file:
+                try:
+                    seq_record = next(record_iter)
+                    SeqIO.write(seq_record, out_handle, "fasta")
+                    count += 1
+                except StopIteration:
+                    break  # 已经读取所有记录，结束循环
+
+
+if __name__ == "__main__":
+    split_fasta(sys.argv[1], sys.argv[2], sys.argv[3])
